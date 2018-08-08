9d3870ea22c24e58d75e165bccaa876470a7a1f3
Gina Lee <ginalee1114@gmail.com>
2018-08-08 23:23:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   def self.genre_count 
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count.has.keys(genre)
+      if genre_count.has_keys?(genre)
         genre_count[genre] += 1 
       else 
         genre_count << genre_count[genre] => 1
