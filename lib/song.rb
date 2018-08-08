163b477d956d3fe63ef0ada897e386914c88b23a
Gina Lee <ginalee1114@gmail.com>
2018-08-08 23:31:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,9 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
     @@count += 1
     @@genres << self.genre
     @@artists << self.artist
